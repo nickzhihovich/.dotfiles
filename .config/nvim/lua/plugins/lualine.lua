0c041d3f5077a138551ae87711d24ab05cdda4cd
Nick Zhihovich <nickzhihovich@gmail.com>
2025-07-06 16:24:22 +0000

    update nvim config

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, {
+        function()
+          return "😄"
+        end,
+      })
+    end,
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return {}
+    end,
+  },
+}
